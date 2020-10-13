8e92b424825240961d29be84ab41e5067661d05d
William Trindade <1#@799wmt>
2020-10-13 00:23:37 -0300

    feat: CRUD finalization

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,5 +5,26 @@ module.exports = {
     async index(req, res) {
         const products = await Product.find();
         return res.json(products);
-    }
+    },
+
+    async create(req, res) {
+        const product = await Product.create(req.body);
+        return res.json(product);
+    },
+
+    async read(req, res) {
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json(product);
+    },
+
+    async delete(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+        return res.send();
+    },
+
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,9 @@ const ProductController = require('./controllers/ProductController');
 
 // Routes
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.create);
+routes.get('/products/:id', ProductController.read);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.delete);
 
 module.exports = routes
