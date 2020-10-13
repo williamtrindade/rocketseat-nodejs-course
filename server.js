c368dcaab77c2fa40d7a33ff986d7ddeda15dc5f
William Trindade <1#@799wmt>
2020-10-13 00:09:07 -0300

    feat: creating controller and routes

--- server.js
+++ server.js
@@ -9,16 +9,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
 requireDir('./src/models');
-const Product = mongoose.model('Product');
 
-// Routes
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'Laravel',
-        description: 'Build like a artisan',
-        url: 'http://github.com/laravel'
-    });
-    return res.send('Produto criado!');
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
