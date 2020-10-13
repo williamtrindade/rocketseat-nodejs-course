453245f3c521bd29900ba0d3701ed214e6494ac3
William Trindade <williamtrindade777@gmail.com>
2020-10-13 00:37:18 -0300

    feat: Fix cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Starting app
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Starting mongo db
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
